bd1dfc280f91d05c6dc5d3d44ebb60e6a1b6960b
Kayla <6lackmacaroon@gmail.com>
2019-04-12 12:00:22 -0600

    barely started on stretch carousel class constructor

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,17 @@
 class Carousel {
+    constructor(banana){
+        this.banana = banana;
+        this.leftButt = this.banana.querySelector('.left-button');
+        this.rightButt = this.banana.querySelector('.right-button');
+        this.data = this.banana.dataset.img;
+        this.pic = this.banana.querySelectorAll(`.carousel img[data-img='${this.data}']`);
+
+    }
 
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector('.carousel');
+
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
